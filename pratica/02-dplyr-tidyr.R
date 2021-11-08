74fad448fe83cbeeb3ba9e908ce55c79004fd542
C. Lente <7017340+clente@users.noreply.github.com>
2021-11-08 18:46:23 -0300

    Compila aula 2

--- pratica/02-dplyr-tidyr.R
+++ pratica/02-dplyr-tidyr.R
@@ -1,3 +1,75 @@
+# Motivação: ver quais colunas possuem NAs e quantos
+
+library(dplyr)
+
+casas <- readr::read_rds("data/casas.rds")
+
+View(casas)
+
+# Vendo colunas com NA
+
+casas %>%
+  summarise(
+    across(
+      .cols = everything(),
+      ~sum(is.na(.x))
+    )
+  ) %>%
+  View()
+
+# Deixando no formato longo
+
+casas %>%
+  summarise(
+    across(
+      .cols = everything(),
+      ~sum(is.na(.x))
+    )
+  ) %>%
+  tidyr::pivot_longer(
+    cols = everything(),
+    names_to = "variavel",
+    values_to = "num_na"
+  ) %>%
+  arrange(desc(num_na))
+
+# -------------------------------------------------------------------------
+# Motivação: ver o número de categorias
+# distintas em cada variável categórica ***
+
+library(dplyr)
+
+casas <- readr::read_rds("data/casas.rds")
+
+casas %>%
+  select(where(is.character)) %>%
+  summarise(
+    across(
+      .fns = n_distinct
+    )
+  ) %>%
+  tidyr::pivot_longer(
+    cols = everything(),
+    names_to = "variavel",
+    values_to = "num_categs"
+  ) %>%
+  arrange(desc(num_categs))
+
+
+# -------------------------------------------------------------------------
+# Motivação: substituir todos os NAs das variáveis
+# categóricas por "sem informação" ***
+
+casas %>%
+  mutate(
+    across(
+      where(is.character),
+      tidyr::replace_na,
+      replace = "sem informação"
+    )
+  ) %>% View()
+
+
 # -------------------------------------------------------------------------
 
 # Motivação: Descobrir o ator com o maior lucro médio na base IMDB,
